e09c1bed5bc526158fefe82635d70c58be45feb6
Emran Hossain <emran.shuvo.dev@gmail.com>
2024-09-01 19:45:15 +0600

    train controller added and train route added in the server file

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/', (req, res) => {
 // api endpoints
 app.use('/api/users', userRoutes);
 app.use('/api/stations', stationRoutes);
+app.use('/api/trains', trainRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
