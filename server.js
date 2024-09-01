4129dea10e2cdca9f0a5ef8d76c11f9ad513e016
Emran Hossain <emran.shuvo.dev@gmail.com>
2024-09-01 18:46:12 +0600

    added db.js file and successfully connected with mongodb

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
 
+connectDB();
 
 app.use(express.json());
 app.get('/', (req, res) => {
