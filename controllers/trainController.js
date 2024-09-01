16ad01d71af69a943fd73fb87064f17e683661e8
Emran Hossain <emran.shuvo.dev@gmail.com>
2024-09-01 19:48:58 +0600

    update and create controller added for trains

---
+++ controllers/trainController.js
@@ -0,0 +1,24 @@
+const Train = require('../models/Train');
+
+
+exports.createTrain = async (req, res) => {
+    const { name, stops } = req.body;
+    try {
+        const train = new Train({ name, stops });
+        await train.save();
+        res.status(201).json(train);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.updateTrain = async (req, res) => {
+    const { id } = req.params;
+    const { name, stops } = req.body;
+    try {
+        const train = await Train.findByIdAndUpdate(id, { name, stops }, { new: true });
+        res.json(train);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
