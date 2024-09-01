de3d128ed52850c4c81795f99ce002f1039d1bf0
Emran Hossain <emran.shuvo.dev@gmail.com>
2024-09-01 19:41:04 +0600

    train schema created

--- models/Train.js
+++ models/Train.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+const TrainSchema = new mongoose.Schema({
+    name: String,
+    stops: [{
+        station: { type: mongoose.Schema.Types.ObjectId, ref: 'Station' },
+        arrivalTime: Date,
+        departureTime: Date,
+    }],
+});
+module.exports = mongoose.model('Train', TrainSchema);
