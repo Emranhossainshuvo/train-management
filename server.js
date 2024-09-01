5dd8557db476156b0905b2d53cfd034d65e880ff
Emran Hossain <emran.shuvo.dev@gmail.com>
2024-09-01 19:19:06 +0600

    route for the station added in the server file

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 });
 // api endpoints
 app.use('/api/users', userRoutes);
+app.use('/api/stations', stationRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
