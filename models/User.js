3e90635a57a123de0f684a740ae4ea0ddf5e1419
Emran Hossain <emran.shuvo.dev@gmail.com>
2024-09-01 18:49:18 +0600

    created user Schema

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    wallet: { type: Number, default: 0 },
+});
+module.exports = mongoose.model('User', UserSchema);
